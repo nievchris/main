0022e3e1dddfc64e5bb2033a4a23ed78d4529a45
Christropher Nieves <108899945+nievchris@users.noreply.github.com>
2023-09-08 11:28:21 -0400

    Update script.js
    Added JavaScript for a sticky header on scroll.

--- script.js
+++ script.js
@@ -2,12 +2,15 @@
 window.addEventListener("scroll", () => {
     const header = document.querySelector("header");
     const logo = document.querySelector(".logo img");
+    const sticky = document.querySelector("sticky-header");
 
     if (window.scrollY > 50) {
         header.classList.add("shrink");
         logo.classList.add("shrink-logo");
+        sticky.classList.add("sticky-header");
     } else {
         header.classList.remove("shrink");
         logo.classList.remove("shrink-logo");
+        sticky.classList.remove("sticky-header");
     }
 });
