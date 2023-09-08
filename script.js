313313c5f9ee554aaee6fa71abee3e7b0efba297
Christropher Nieves <108899945+nievchris@users.noreply.github.com>
2023-09-08 12:38:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,15 @@
 window.addEventListener("scroll", () => {
     const header = document.querySelector("header");
     const logo = document.querySelector(".logo img");
-    const sticky = document.querySelector("sticky-header");
+    const stickyHeader = document.querySelector(".sticky-header");
 
-    if (window.scrollY > 50) {
+    if (window.scrollY > 60) {
         header.classList.add("shrink");
         logo.classList.add("shrink-logo");
-        sticky.classList.add("sticky-header");
+        stickyHeader.classList.add("shrink");
     } else {
         header.classList.remove("shrink");
         logo.classList.remove("shrink-logo");
-        sticky.classList.remove("sticky-header");
+        stickyHeader.classList.remove("shrink");
     }
 });
